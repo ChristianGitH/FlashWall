ad707af4cf5c165cff0cedf859241d9cb2af1817
ChristianGitH <christianc.dev@gmail.com>
2025-12-16 16:10:24 +0100

    slideshow : changed avatar size and caption content show delay
    Changes to be committed:
    	modified:   resources/views/livewire/displaywalls/slideshow.blade.php

--- resources/views/livewire/displaywalls/slideshow.blade.php
+++ resources/views/livewire/displaywalls/slideshow.blade.php
@@ -375,7 +375,7 @@ public function markImageAsDisplayed($imageId, $nextImageId)
                             x-init="
                             const duration = {{ $displaySettings['duration'] }};
                             showCaptionDelay = duration/2;
-                            showCaptionContentDelay = showCaptionDelay+700
+                            showCaptionContentDelay = showCaptionDelay+750
                             if ({{ $index }} === 0) {
                                 setTimeout(() => showCaption = true, showCaptionDelay);
                                 setTimeout(() => showCaptionContent = true, showCaptionContentDelay);
@@ -399,16 +399,11 @@ public function markImageAsDisplayed($imageId, $nextImageId)
 
                             <div class="flex justify-center items-center gap-2">
                                 @if($wall->require_avatar_submitter && $image->submitter_avatar)
-                                <span
-                                    class="emoji_font bg-base-300 rounded-full inline-flex items-center justify-center"
-                                    style="font-size: 0.75em;
-                                        width: 1.7em;
-                                        height: 1.7em;
-                                    "
-                                >{{ $image->submitter_avatar }}</span>
+                                <span class="emoji_font bg-base-300 rounded-full inline-flex items-center justify-center"
+                                    style="font-size: 0.75em; width: 1.7em; height: 1.7em;">{{ $image->submitter_avatar }}</span>
                                 @endif
                                 @if($wall->submitter_name_on_wall && $image->submitter_name)
-                                    <span style="ml-1 font-size: {{ $displaySettings['submitter_name_font_size'] . $displaySettings['caption_font_unit'] }};
+                                    <span class="ml-2 uppercase" style="font-size: {{ $displaySettings['submitter_name_font_size'] . $displaySettings['caption_font_unit'] }};
                                     color: {{ $displaySettings['submitter_name_font_color'] }};
                                     ">
                                         {{ $image->submitter_name }}
